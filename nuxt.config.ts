ebfe0ab12e778e2fe10aa9ac697106065239f2c5
Martijn <512214@student.fontys.nl>
2024-06-14 19:40:29 +0200

    Update manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,40 +6,30 @@ export default defineNuxtConfig({
     manifest: {
       "name": "Spullekes.net",
       "short_name": "Spullekes",
+      "description": "Een plek voor jouw tweede hands spullekes",
       "start_url": ".",
       "display": "standalone",
-      "background_color": "#fff",
-      "description": "A readable Hacker News app.",
-      "icons": [
+      "background_color": "#12355B",
+      icons: [
         {
-          "src": "images/touch/homescreen48.png",
-          "sizes": "48x48",
-          "type": "image/png"
+          src: "public/android-chrome-192x192.png",
+          sizes: "64x64",
+          type: "image/png",
         },
         {
-          "src": "images/touch/homescreen72.png",
-          "sizes": "72x72",
-          "type": "image/png"
+          src: "public/android-chrome-192x192.png",
+          sizes: "144x144",
+          type: "image/png",
         },
         {
-          "src": "images/touch/homescreen96.png",
-          "sizes": "96x96",
-          "type": "image/png"
+          src: "public/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
         },
         {
-          "src": "images/touch/homescreen144.png",
-          "sizes": "144x144",
-          "type": "image/png"
-        },
-        {
-          "src": "images/touch/homescreen168.png",
-          "sizes": "168x168",
-          "type": "image/png"
-        },
-        {
-          "src": "images/touch/homescreen192.png",
-          "sizes": "192x192",
-          "type": "image/png"
+          src: "public/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
         },
       ]
     }
@@ -60,7 +50,8 @@ export default defineNuxtConfig({
       }
     }
   }
-})
+}
+)
 
 
 // "icons": [
