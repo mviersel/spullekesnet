42f36bf246eb7ce97dd96adb155dd03e030f5116
Martijn <119597405+mviersel@users.noreply.github.com>
2024-06-14 12:04:22 +0200

    Aanmaken pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', "@vite-pwa/nuxt"],
+  pwa: {
+    "name": "Spullekesnet",
+    "short_name": "Spullekes",
+    "icons": [
+      {
+        "src": "/android-chrome-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "/android-chrome-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ],
+    "theme_color": "#ffffff",
+    "background_color": "#ffffff",
+    "display": "standalone"
+  },
   plugins: ["~/plugins/firebaseConfig.js"],
   tailwindcss: {
     config: {
