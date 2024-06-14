6fb04ef1c5a2b88780d229c0f85961ea404c22a8
Martijn <512214@student.fontys.nl>
2024-06-14 16:59:30 +0200

    Manifest debuggen en info toevoegen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,23 +3,46 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', "@vite-pwa/nuxt"],
   pwa: {
-    "name": "Spullekesnet",
-    "short_name": "Spullekes",
-    "icons": [
-      {
-        "src": "/android-chrome-192x192.png",
-        "sizes": "192x192",
-        "type": "image/png"
-      },
-      {
-        "src": "/android-chrome-512x512.png",
-        "sizes": "512x512",
-        "type": "image/png"
-      }
-    ],
-    "theme_color": "#ffffff",
-    "background_color": "#ffffff",
-    "display": "standalone"
+    manifest: {
+      "name": "Spullekes.net",
+      "short_name": "Spullekes",
+      "start_url": ".",
+      "display": "standalone",
+      "background_color": "#fff",
+      "description": "A readable Hacker News app.",
+      "icons": [
+        {
+          "src": "images/touch/homescreen48.png",
+          "sizes": "48x48",
+          "type": "image/png"
+        },
+        {
+          "src": "images/touch/homescreen72.png",
+          "sizes": "72x72",
+          "type": "image/png"
+        },
+        {
+          "src": "images/touch/homescreen96.png",
+          "sizes": "96x96",
+          "type": "image/png"
+        },
+        {
+          "src": "images/touch/homescreen144.png",
+          "sizes": "144x144",
+          "type": "image/png"
+        },
+        {
+          "src": "images/touch/homescreen168.png",
+          "sizes": "168x168",
+          "type": "image/png"
+        },
+        {
+          "src": "images/touch/homescreen192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+      ]
+    }
   },
   plugins: ["~/plugins/firebaseConfig.js"],
   tailwindcss: {
@@ -38,3 +61,37 @@ export default defineNuxtConfig({
     }
   }
 })
+
+
+// "icons": [
+//   {
+//     "src": "images/touch/homescreen48.png",
+//     "sizes": "48x48",
+//     "type": "image/png"
+//   },
+//   {
+//     "src": "images/touch/homescreen72.png",
+//     "sizes": "72x72",
+//     "type": "image/png"
+//   },
+//   {
+//     "src": "images/touch/homescreen96.png",
+//     "sizes": "96x96",
+//     "type": "image/png"
+//   },
+//   {
+//     "src": "images/touch/homescreen144.png",
+//     "sizes": "144x144",
+//     "type": "image/png"
+//   },
+//   {
+//     "src": "images/touch/homescreen168.png",
+//     "sizes": "168x168",
+//     "type": "image/png"
+//   },
+//   {
+//     "src": "images/touch/homescreen192.png",
+//     "sizes": "192x192",
+//     "type": "image/png"
+//   }
+// ],
