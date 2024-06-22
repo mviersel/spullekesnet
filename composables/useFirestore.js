c6428cf771d692f5eff8eace515bc4782c5377e9
Martijn <512214@student.fontys.nl>
2024-06-22 20:01:48 +0200

    Logica aanpassen

--- composables/useFirestore.js
+++ composables/useFirestore.js
@@ -2,7 +2,11 @@
 import { useNuxtApp } from "#app";
 import {
     collection,
+    addDoc,
     getDocs,
+    updateDoc,
+    deleteDoc,
+    doc,
 } from "firebase/firestore";
 
 const useFirestore = () => {
@@ -14,8 +18,26 @@ const useFirestore = () => {
         return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
     };
 
+    const addItem = async (product) => {
+        const docRef = await addDoc(collectionRef, product);
+        return docRef.id;
+    };
+
+    const updateItem = async (id, updatedProduct) => {
+        const docRef = doc($db, "products", id);
+        await updateDoc(docRef, updatedProduct);
+    };
+
+    const deleteItem = async (id) => {
+        const docRef = doc($db, "products", id);
+        await deleteDoc(docRef);
+    };
+
     return {
         getItems,
+        addItem,
+        updateItem,
+        deleteItem,
     };
 };
 
