5696c9d5b956723acf88d44aae21b43f5846872d
Martijn <512214@student.fontys.nl>
2024-06-23 16:41:31 +0200

    Overlay maken voor aanpassen van product

--- components/OverviewMobile.vue
+++ components/OverviewMobile.vue
@@ -1,59 +1,63 @@
 <!-- pages/index.vue -->
 <template>
-    <div v-for="product in products" :key="product.id">
-        <div class="border-b-2 m-4 pb-2 overflow-hidden md:w-80">
-            <img class="h-48 w-full object-cover object-center" :src="`${product.image}`" alt="Product Image" />
-            <div class="overview-border">{{ product.title }}</div>
-            <div class="overview-border">{{ product.description }}</div>
-            <div class="overview-border">{{ product.oldPrice }}</div>
-            <div class="overview-border">{{ product.newPrice }}</div>
-            <div class="overview-border">{{ product.image }}</div>
-            <div class="flex justify-center space-x-8 pt-4">
-                <button @click="editItem(product)"
-                    class="middle none center rounded-lg bg-orange-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-orange-500/20 transition-all hover:shadow-lg hover:shadow-orange-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                    data-ripple-light="true">Edit product</button>
-                <button @click="removeItem(product.id)"
-                    class="middle none center mr-4 rounded-lg bg-red-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                    data-ripple-light="true">Remove product</button>
-            </div>
-            <!-- edit product -->
-            <div>
-                <div id="editForm" style="display: none;" class="p-6">
-                    <Label class="overview-border font-bold">Title</Label>
-                    <input class="overview-border" type="text" id="title" name="title">
-                    <label class="overview-border font-bold">Descirption</label>
-                    <input class="overview-border" type="text" id="description" name="description">
-                    <label class="overview-border font-bold">Old price</label>
-                    <input class="overview-border" type="text" id="oldPrice" name="oldPrice">
-                    <label class="overview-border font-bold">New price</label>
-                    <input class="overview-border" type="text" id="newPrice" name="newPrice">
-                    <label class="overview-border font-bold">Image</label>
-                    <input class="overview-border" type="text" id="image" name="image">
+    <div class="">
+        <div class=" md:grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-x-5">
+            <div v-for="product in products" :key="product.id">
+                <div class="border-b-2 p-4 m-auto pb-2 overflow-hidden md:w-80">
+                    <img class="h-48 w-full object-cover object-center" :src="`${product.image}`" alt="Product Image" />
+                    <div class="overview-border">{{ product.title }}</div>
+                    <div class="overview-border">{{ product.description }}</div>
+                    <div class="overview-border">{{ product.oldPrice }}</div>
+                    <div class="overview-border">{{ product.newPrice }}</div>
+                    <div class="overview-border">{{ product.image }}</div>
                     <div class="flex justify-center space-x-8 pt-4">
-                        <button id="saveButton"
-                            class="middle none center rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-orange-500/20 transition-all hover:shadow-lg hover:shadow-orange-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                            data-ripple-light="true">Save</button>
-                        <button id="cancelButton"
+                        <button @click="editItem(product)"
+                            class="middle none center rounded-lg bg-orange-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-orange-500/20 transition-all hover:shadow-lg hover:shadow-orange-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            data-ripple-light="true">Bewerken</button>
+                        <button @click="removeItem(product.id)"
                             class="middle none center mr-4 rounded-lg bg-red-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                            data-ripple-light="true">Cancel</button>
+                            data-ripple-light="true">Verwijderen</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="w-fit mt-8 ml-8">
-
-        <!-- Verandering UI -->
-
-        <!-- new item form -->
-        <div>
-            <h1>Add Item</h1>
-            <input required v-model="newItem.title" placeholder="New title" />
-            <input required v-model="newItem.description" placeholder="New description" />
-            <input required v-model="newItem.oldPrice" placeholder="New old price" />
-            <input required v-model="newItem.newPrice" placeholder="New new price" />
-            <input required v-model="newItem.image" placeholder="New image" />
+    <!-- edit product overlay -->
+    <div id="editForm" style="display: none; ">
+        <div class="fixed top-0 left-0 bg-white z-10 w-screen h-screen flex justify-center items-center rounded-md">
+            <div class="m-5 w-96">
+                <p class="overview-border font-bold">Title</p>
+                <input class="overview-border" type="text" id="title" name="title">
+                <p class="overview-border font-bold">Descirption</p>
+                <input class="overview-border" type="text" id="description" name="description">
+                <p class="overview-border font-bold">Old price</p>
+                <input class="overview-border" type="text" id="oldPrice" name="oldPrice">
+                <p class="overview-border font-bold">New price</p>
+                <input class="overview-border" type="text" id="newPrice" name="newPrice">
+                <p class="overview-border font-bold">Image</p>
+                <input class="overview-border" type="text" id="image" name="image">
+                <div class="flex justify-center space-x-8 pt-4">
+                    <button id="saveButton"
+                        class="middle none center rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-orange-500/20 transition-all hover:shadow-lg hover:shadow-orange-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        data-ripple-light="true">Opslaan</button>
+                    <button id="cancelButton"
+                        class="middle none center mr-4 rounded-lg bg-red-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        data-ripple-light="true">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- new item form -->
+    <div class="bg-gray-200">
+        <div class="flex flex-col m-5">
+            <h1 class="">Add Item</h1>
+            <input class="bg-inherit overview-border" required v-model="newItem.title" placeholder="New title" />
+            <input class="bg-inherit overview-border" required v-model="newItem.description"
+                placeholder="New description" />
+            <input class="bg-inherit overview-border" required v-model="newItem.oldPrice" placeholder="New old price" />
+            <input class="bg-inherit overview-border" required v-model="newItem.newPrice" placeholder="New new price" />
+            <input class="bg-inherit overview-border" required v-model="newItem.image" placeholder="New image" />
             <button @click="addItemHandler">Add Item</button>
         </div>
     </div>
